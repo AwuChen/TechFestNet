e90d79c1d1d739a083eed934c390ad287dc75d0c
Awu Chen <awwu@awus-mbp.media.mit.edu>
2026-04-08 19:32:51 -0400

    Updates

---
+++ precache-manifest.147fd9b0add6766c4e2c2de998be552b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3af4ce59ec20021d8b844ca5e12c87",
+    "url": "/TechFestNet/index.html"
+  },
+  {
+    "revision": "34c4344ef152bd550259",
+    "url": "/TechFestNet/static/css/main.ead59979.chunk.css"
+  },
+  {
+    "revision": "0f83b924877a1a9d0dcb",
+    "url": "/TechFestNet/static/js/2.58140313.chunk.js"
+  },
+  {
+    "revision": "7b36c4a4b00d4c1eb1f21a15ccb34795",
+    "url": "/TechFestNet/static/js/2.58140313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c4344ef152bd550259",
+    "url": "/TechFestNet/static/js/main.16180284.chunk.js"
+  },
+  {
+    "revision": "547257aca033a056b479",
+    "url": "/TechFestNet/static/js/runtime-main.24ae53c3.js"
+  }
+]);
